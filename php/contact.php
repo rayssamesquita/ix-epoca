732e48de30783a690f3b902d313aac85f25735d4
rayssamesquita <32690050+rayssamesquita@users.noreply.github.com>
2017-10-25 02:09:28 -0200

    Commit Inicial

---
+++ php/contact.php
@@ -0,0 +1,107 @@
+<?php
+/*
+* Contact Form Class
+*/
+
+
+header('Cache-Control: no-cache, must-revalidate');
+header('Expires: Mon, 26 Jul 1997 05:00:00 GMT');
+header('Content-type: application/json');
+
+$admin_email = 'gracielly_fernandes23@hotmail.com'; // Your Email
+$message_min_length = 5; // Min Message Length
+
+class Contact_Form{
+	function __construct($details, $email_admin, $message_min_length){
+		
+		$this->name = stripslashes($details['name']);
+		$this->email = trim($details['email']);
+		$this->subject = 'Contato - EPOCA 2016'; // Subject 
+		$this->message = stripslashes($details['message']);
+	
+		$this->email_admin = $email_admin;
+		$this->message_min_length = $message_min_length;
+		
+		$this->response_status = 1;
+		$this->response_html = '';
+	}
+
+
+	private function validateEmail(){
+		$regex = '/^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i';
+	
+		if($this->email == '') { 
+			return false;
+		} else {
+			$string = preg_replace($regex, '', $this->email);
+		}
+	
+		return empty($string) ? true : false;
+	}
+
+
+	private function validateFields(){
+		// Check name
+		if(!$this->name)
+		{
+			$this->response_html .= '<p>Por favor, informe seu nome.</p>';
+			$this->response_status = 0;
+		}
+
+		// Check email
+		if(!$this->email)
+		{
+			$this->response_html .= '<p>Por favor, informe seu e-mail.</p>';
+			$this->response_status = 0;
+		}
+		
+		// Check valid email
+		if($this->email && !$this->validateEmail())
+		{
+			$this->response_html .= '<p>Por favor, informe um e-mail válido.</p>';
+			$this->response_status = 0;
+		}
+		
+		// Check message length
+		if(!$this->message || strlen($this->message) < $this->message_min_length)
+		{
+			$this->response_html .= '<p>Por favor, insira sua mensagem. Deverá conter, no mínimo, '.$this->message_min_length.' caracteres.</p>';
+			$this->response_status = 0;
+		}
+	}
+
+
+	private function sendEmail(){
+		$mail = mail($this->email_admin, $this->subject, $this->message,
+			 "From: ".$this->name." <".$this->email.">\r\n"
+			."Reply-To: ".$this->email."\r\n"
+		."X-Mailer: PHP/" . phpversion());
+	
+		if($mail)
+		{
+			$this->response_status = 1;
+			$this->response_html = '<p>Obrigado!</p>';
+		}
+	}
+
+
+	function sendRequest(){
+		$this->validateFields();
+		if($this->response_status)
+		{
+			$this->sendEmail();
+		}
+
+		$response = array();
+		$response['status'] = $this->response_status;	
+		$response['html'] = $this->response_html;
+		
+		echo json_encode($response);
+	}
+}
+
+
+$contact_form = new Contact_Form($_POST, $admin_email, $message_min_length);
+$contact_form->sendRequest();
+
+?>
